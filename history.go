53e075e780f36f2703e9b5d4bd733b5d9c8982f5
praveen <praveenraj9495@gmail.com>
2026-08-16 13:05:43 +0530

    fix: support options on NewFileHistory

--- history.go
+++ history.go
@@ -4,11 +4,11 @@ import "github.com/hastekit/hastekit-sdk-go/pkg/agents/history"
 
 type History = history.CommonConversationManager
 
-func NewFileHistory(path string) *History {
+func NewFileHistory(path string, opts ...history.ConversationManagerOptions) *History {
 	p, err := history.NewFileConversationPersistence(path)
 	if err != nil {
 		panic(err)
 	}
 
-	return history.NewConversationManager(p)
+	return history.NewConversationManager(p, opts...)
 }
